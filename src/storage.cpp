066b1d3ea0eec4f375a365eb173a05b5539cd146
Husain206 <husainal518@gmail.com>
2026-04-18 04:00:33 +0300

    display more information: description and due_date

--- src/storage.cpp
+++ src/storage.cpp
@@ -2,6 +2,12 @@
 #include <fstream>
 #include <string>
 
+static str sanitize_field(str s){
+  for(char& c : s){
+    if(c == '|') c = '/';
+  }
+  return s;
+}
 
 vec<Todo> load_todos(const str &path){
   vec<Todo> todos;
@@ -15,7 +21,7 @@ vec<Todo> load_todos(const str &path){
 
   while(std::getline(in, line)){
     auto parts = split(line, '|');
-    if(parts.size() < 6) continue;
+    if(parts.size() < 8) continue;
 
     Todo t;
     t.id = std::stoi(parts[0]);
@@ -24,6 +30,8 @@ vec<Todo> load_todos(const str &path){
     t.category = parts[3];
     t.title = parts[4];
     t.created_at = parts[5];
+    t.description = parts[6];
+    t.due_date = parts[7];
 
     todos.push_back(t);
   }
@@ -33,13 +41,25 @@ vec<Todo> load_todos(const str &path){
 void save_todos(const str &path, const vec<Todo> &todos){
   std::ofstream out(path);
 
-  for(const auto& t : todos){
-    out << t.id << "|"
-        << (t.done ? 1 : 0) << "|"
-        << (int)t.priority << "|"
-        << t.category << "|"
-        << t.title << "|"
-        << t.created_at << "\n";
+  // for(const auto& t : todos){
+  //   out << t.id << "|"
+  //       << (t.done ? 1 : 0) << "|"
+  //       << (int)t.priority << "|"
+  //       << t.category << "|"
+  //       << t.title << "|"
+  //       << t.created_at << "\n";
+  // }
+
+   for(const auto& t : todos){
+    out << t.id << '|'
+        << (t.done ? 1 : 0) << '|'
+        << (int)t.priority << '|'
+        << t.category << '|'
+        << t.title << '|'
+        << t.created_at << '|'
+        << sanitize_field(t.description) << '|'
+        << t.due_date
+        << '\n';
   }
 }
 
